1e132a5b73e4eceefb183d0c2771bf02ae0e1117
Vasiliy Tyubek <v.tyubek@gmail.com>
2015-12-04 15:22:54 +0300

    Updated gitlab webhooks definition

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -5,59 +5,12 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/fsouza/go-dockerclient"
 	"github.com/go-macaron/binding"
+	"github.com/vasiliy-t/blacksmith/gitlab"
 	"gopkg.in/macaron.v1"
 	"gopkg.in/redis.v3"
 	"log"
 	"net/http"
 )
-
-//GitlabWebHook is a basic struct representing any gitlab webhook payload
-type GitlabWebHook struct {
-	ObjectKind string `json:"object_kind"`
-}
-
-//Repository represents gitlab repo info from webhook payload
-type Repository struct {
-	Name            string `json:"name"`
-	URL             string `json:"url"`
-	Description     string `json:"description"`
-	Homepage        string `json:"homepage"`
-	GitHTTPURL      string `json:"git_http_url"`
-	GitSSHURL       string `json:"git_ssh_url"`
-	VisibilityLevel int    `json:"visibility_level"`
-}
-
-//Commit represents gitlab commit info from webhook payload
-type Commit struct {
-	ID        string `json:"id"`
-	Message   string `json:"message"`
-	Timestamp string `json:"timestamp"`
-	URL       string `json:"url"`
-	Author    User   `json:"author"`
-}
-
-//User represents gitlab user info from webhook payload
-type User struct {
-	Name      string `json:"name"`
-	Email     string `json:"email"`
-	Username  string `json:"username"`
-	AvatarURL string `json:"avatar_url"`
-}
-
-//GitlabPushRequest represents gitlab push notification payload
-type GitlabPushRequest struct {
-	GitlabWebHook
-	Before            string     `json:"before"`
-	After             string     `json:"after"`
-	Ref               string     `json:"ref"`
-	UserID            int        `json:"user_id"`
-	UserName          string     `json:"user_name"`
-	UserEmail         string     `json:"user_email"`
-	ProjectID         int        `json:"project_id"`
-	TotalCommitsCount int        `json:"total_commits_count"`
-	Commits           []Commit   `json:"commits"`
-	Repository        Repository `json:"repository"`
-}
 
 //DaemonCmd is a command to start server in daemon mode
 var DaemonCmd = cli.Command{
@@ -109,7 +62,7 @@ func daemon(c *cli.Context) {
 	m.Use(macaron.Logger())
 	m.Map(redisClient)
 	m.Map(client)
-	m.Post("/push", binding.Json(GitlabPushRequest{}), func(gpr GitlabPushRequest, client *docker.Client) string {
+	m.Post("/push", binding.Json(&gitlab.Push{}), func(gpr gitlab.Push, client *docker.Client) string {
 		gitURL := fmt.Sprintf("REPOSITORY_GIT_HTTP_URL=%s", gpr.Repository.GitHTTPURL)
 		ref := fmt.Sprintf("REF=%s", gpr.Ref)
 		commit := fmt.Sprintf("AFTER=%s", gpr.After)
---
+++ gitlab/gitlab.go
@@ -0,0 +1,66 @@
+package gitlab
+
+//WebHook is a basic struct representing any gitlab webhook payload
+type WebHook struct {
+	ObjectKind string `json:"object_kind"`
+}
+
+//Repository represents gitlab repo info from webhook payload
+type Repository struct {
+	Name            string `json:"name"`
+	URL             string `json:"url"`
+	Description     string `json:"description"`
+	Homepage        string `json:"homepage"`
+	GitHTTPURL      string `json:"git_http_url"`
+	GitSSHURL       string `json:"git_ssh_url"`
+	VisibilityLevel int    `json:"visibility_level"`
+}
+
+//Commit represents gitlab commit info from webhook payload
+type Commit struct {
+	ID        string `json:"id"`
+	Message   string `json:"message"`
+	Timestamp string `json:"timestamp"`
+	URL       string `json:"url"`
+	Author    User   `json:"author"`
+}
+
+//User represents gitlab user info from webhook payload
+type User struct {
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Username  string `json:"username"`
+	AvatarURL string `json:"avatar_url"`
+}
+
+//Push represents gitlab push notification payload
+type Push struct {
+	WebHook
+	Before            string     `json:"before"`
+	After             string     `json:"after"`
+	Ref               string     `json:"ref"`
+	UserID            int        `json:"user_id"`
+	UserName          string     `json:"user_name"`
+	UserEmail         string     `json:"user_email"`
+	ProjectID         int        `json:"project_id"`
+	Commits           []Commit   `json:"commits"`
+	Repository        Repository `json:"repository"`
+	TotalCommitsCount int        `json:"total_commits_count"`
+	Added             []string   `json:"added"`
+	Modified          []string   `json:"modified"`
+	Removed           []string   `json:"removed"`
+}
+
+//TagPush represents gitlab tag push notification payload
+type TagPush struct {
+	WebHook
+	Before            string     `json:"before"`
+	After             string     `json:"after"`
+	Ref               string     `json:"ref"`
+	UserID            int        `json:"user_id"`
+	UserName          string     `json:"user_name"`
+	ProjectID         int        `json:"project_id"`
+	Repository        Repository `json:"repository"`
+	Commits           []Commit   `json:"commits"`
+	TotalCommitsCount int        `json:"total_commit_count"`
+}
