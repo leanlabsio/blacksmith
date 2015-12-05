8ceb8b71eda6f4efb10a348a4e85d414672943c9
Vasiliy Tyubek <v.tyubek@gmail.com>
2015-12-05 21:09:20 +0300

    Store env vars in redis

--- route/env.go
+++ route/env.go
@@ -3,12 +3,14 @@ package route
 import (
 	"github.com/go-macaron/binding"
 	"gopkg.in/macaron.v1"
+	"gopkg.in/redis.v3"
 	"log"
 )
 
+//Env represents single API payload entry
 type Env struct {
-	EnvName string `json:"envname"`
-	EnvVal  string `json:"envval"`
+	Name  string `json:"name"`
+	Value string `json:"value"`
 }
 
 //PostEnv is an API endpoint to store user defined environment
@@ -16,9 +18,12 @@ type Env struct {
 //POST Content-Type: application/json [{"ENVNAME:ENVVAL"}]
 func PostEnv() []macaron.Handler {
 	return []macaron.Handler{
-		binding.Json(Env{}),
-		func(e Env) string {
-			log.Printf("%+v", e)
+		binding.Json([]Env{}),
+		func(payload []Env, redis *redis.Client) string {
+			for _, v := range payload {
+				status := redis.Set(v.Name, v.Value, 0)
+				log.Printf("%+v", status.Err())
+			}
 			return "Sucess"
 		},
 	}
